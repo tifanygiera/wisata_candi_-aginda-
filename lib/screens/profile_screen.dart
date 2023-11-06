5ea8faf74bab4fec8baebd7ff7d84c522752ad96
tifanygiera <agindanoorfadhillah@mhs.mdp.ac.id>
2023-11-06 11:21:52 +0700

    signupscreen dan signinscreen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -10,7 +10,7 @@ class _ProfileScreenState extends State<ProfileScreen>{
   //TODO: 1. Deklarasikan Varible yang dibutuhkan
   bool isSignedIn = false;
   String fullName = '';
-  Sting userName = '';
+  String userName = '';
   int favoriteCandiCount = 0;
 
   @override
@@ -79,20 +79,50 @@ class _ProfileScreenState extends State<ProfileScreen>{
               SizedBox(height: 4),
               Row(
                 children: [
-                  Icon(Icons.person, color: Colors.blue),
-                  SizedBox(width: 8),
-                  Text('Nama', style: TextStyle(
-                    fontSize: 18, fontWeight: FontWeight.bold,
-                  ),),
+                  SizedBox(width: MediaQuery.of(context).size.width / 3,
+                    child: Row(
+                      children: [
+                        Icon(Icons.person, color: Colors.blue),
+                        SizedBox(width: 8),
+                        Text('Nama', style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold,
+                        ),),
+                      ],
+                    ),),
+                  Expanded(
+                    child: Text(': $fullName', style: TextStyle(
+                        fontSize: 18),),),
+                  if(isSignedIn) Icon(Icons.edit),
                 ],
               ),
-              //TODO: 4. Buaut ProfileActions yang berisikan textButton sign in/ sign out
+              SizedBox(height: 4),
+              Divider(color: Colors.deepPurple[100]),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  SizedBox(width: MediaQuery.of(context).size.width / 3,
+                    child: Row(
+                      children: [
+                        Icon(Icons.person, color: Colors.blue),
+                        SizedBox(width: 8),
+                        Text('Nama', style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold,
+                        ),),
+                      ],
+                    ),),
+                ],
+              ),
+              // TODO: 4. Buat ProfileActions yang berisi TextButton sign in/
+              SizedBox(height: 4),
+              Divider(color: Colors.deepPurple[100]),
+              SizedBox(height: 20),
+              isSignedIn ? TextButton(onPressed: (){}, child: Text('Sign Out'))
+                  : TextButton(onPressed: (){}, child: Text('Sign In')),
             ],
           ),
-        )
-    )
-    ],
-    ),
-    )
+        ),
+          ],
+        ),
+    );
   }
 }
